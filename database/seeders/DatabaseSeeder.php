388cead17e0cbe3abc63e7f7ea7b6811bc5d5249
Hicham <oulaminhicham019@gamil.com>
2024-06-03 20:26:04 +0100

    resrevation seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
             DistinationTableSeeder::class,
             GuideTableSeeder::class ,
             CirquitTableSeeder::class,
+            ResservationTableseeder::class
         ]);
     }
 }
