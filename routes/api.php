960dd49fb67e235b032f82fe0f21eeb6216d9781
Hicham <oulaminhicham019@gamil.com>
2024-06-05 19:33:33 +0100

    reservation apis

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CirquitController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\DistinationController;
 use App\Http\Controllers\GuideeController;
+use App\Http\Controllers\ReservationFController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,8 +26,11 @@ Route::apiResource("/cirquits" , CirquitController::class);
 Route::apiResource('/guides' , GuideeController::class);
 //     ######             CLIENT APIS            #########
 Route::apiResource("/clients",ClientController::class);
+//     ######             reservation apis            #########
+Route::apiResource('reservations' , ReservationFController::class);
 //     ######             LOGIN            #########
 Route::post('/login' , [AuthController::class , 'login']);
+
 Route::post('/logout' , [AuthController::class , 'logout']);
 //   get the cirquit of somme guide
 // Route::get('/guides/{id}' , [GuideeController::class ,'getCirquits']);
